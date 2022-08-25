8f0e823fe873a7eec1d6714d491b8f65db13c89f
anthony <aedaub96@gmail.com>
2022-08-25 14:18:46 -0400

    Completion of the data structure.

--- src/lib.rs
+++ src/lib.rs
@@ -61,4 +61,45 @@ mod tests {
         };
         assert!(rl_duration <= list_duration, "RefList Duration {} <= List Duration {} = {}", rl_duration, list_duration, rl_duration <= list_duration);
     }
+
+    #[test]
+    fn test_input3() {
+        let list = {
+            let mouse_ids = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            let mut keys = ('a'..'t').into_iter().map(|value| Input::Key(value)).collect::<Vec<Input>>();
+            let mut input = mouse_ids.iter().map(|value| Input::MouseButton(*value)).collect::<Vec<Input>>();
+            input.append(&mut keys);
+            input
+        };
+        let ref_list = {
+            let mut ref_list = RefList::new();
+            for value in &list {
+                ref_list.push(value.clone());
+            }
+            ref_list
+        };
+
+        let iterations = 100;
+
+        let list_time = {
+            let instant = Instant::now();
+            for _ in 0..iterations {
+                for value in &list {
+                    let _ = list.contains(value);
+                }
+            }
+            instant.elapsed().as_secs_f32() / iterations as f32
+        };
+        let ref_list_time = {
+            let instant = Instant::now();
+            for _ in 0..iterations {
+                for value in &list {
+                    let _ = ref_list.contains(value);
+                }
+            }
+            instant.elapsed().as_secs_f32() / iterations as f32
+        };
+
+        println!("Average List Contains: {}secs vs Average RefList Contains: {}secs", list_time, ref_list_time);
+    }
 }
